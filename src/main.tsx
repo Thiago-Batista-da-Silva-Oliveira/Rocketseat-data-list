5d3546ee5da01349dcd323d0067a108f10bd2599
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-02-21 23:54:50 -0300

    feat: create-tag-form

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { App } from './App'
 
 const queryClient = new QueryClient();
 
